77537dfa59bcf959b997b2183607289cdd6e6d35
rolandvgi <roland.vgi@gmail.com>
2020-03-08 19:16:10 +0200

    added carousel and finished with dropdown menu(colors must be fixed)

---
+++ js.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        items: 1,
+        autoplay: true,
+        loop:true,
+        animateOut: 'fadeOut' 
+    });
+   
+  });
